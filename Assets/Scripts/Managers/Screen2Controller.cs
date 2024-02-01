9995fadc3abf3fe6dd7d8cad3888fe720397911f
Daniel Mardunovich <mardunovich@gmail.com>
2024-02-01 17:12:32 +0100

    Added fail and succeed images for quicktime event.
    Also removed the white quare backgrounds

--- Assets/Scripts/Managers/Screen2Controller.cs
+++ Assets/Scripts/Managers/Screen2Controller.cs
@@ -1,11 +1,14 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Screen2Controller : MonoBehaviour
 {
     public Sprite[] picPlacePics;
+    public Sprite[] picBackgroundPics;
     public GameObject[] picPlaces;
 
     public GameObject[] playerLivesImages, enemyLivesImages;
@@ -41,7 +44,7 @@ public class Screen2Controller : MonoBehaviour
         {
             sucessess[i] = false;
             picPlaces[i].transform.GetChild(0).gameObject.SetActive(false);
-            picPlaces[i].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.blue;
+            picPlaces[i].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[0];
             switch (Random.Range(0, 10))
             {
                 case <5:
@@ -73,34 +76,34 @@ public class Screen2Controller : MonoBehaviour
     {
         switch (step)
         {
-            case 4:
+            case 3:
                 if (picPlaces[0].GetComponent<SpriteRenderer>().sprite == picPlacePics[1])
                 {
-                    picPlaces[0].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[0].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[0] = true;
                     EventManager.Instance.AddScore(25);
                 }
                 break;
-            case 5:
+            case 4:
                 if (picPlaces[1].GetComponent<SpriteRenderer>().sprite == picPlacePics[1])
                 {
-                    picPlaces[1].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[1].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[1] = true;
                     EventManager.Instance.AddScore(25);
                 }
                 break;
-            case 6:
+            case 5:
                 if (picPlaces[2].GetComponent<SpriteRenderer>().sprite == picPlacePics[1])
                 {
-                    picPlaces[2].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[2].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[2] = true;
                     EventManager.Instance.AddScore(25);
                 }
                 break;
-            case 7:
+            case 6:
                 if (picPlaces[3].GetComponent<SpriteRenderer>().sprite == picPlacePics[1])
                 {
-                    picPlaces[3].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[3].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[3] = true;
                     EventManager.Instance.AddScore(25);
                 }
@@ -112,36 +115,36 @@ public class Screen2Controller : MonoBehaviour
     {
         switch (step)
         {
-            case 4:
+            case 3:
                 if (picPlaces[0].GetComponent<SpriteRenderer>().sprite == picPlacePics[0])
                 {
-                    picPlaces[0].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[0].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[0] = true;
                     EventManager.Instance.AddScore(25);
 
                 }
                 break;
-            case 5:
+            case 4:
                 if (picPlaces[1].GetComponent<SpriteRenderer>().sprite == picPlacePics[0])
                 {
-                    picPlaces[1].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[1].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[1] = true;
                     EventManager.Instance.AddScore(25);
 
                 }
                 break;
-            case 6:
+            case 5:
                 if (picPlaces[2].GetComponent<SpriteRenderer>().sprite == picPlacePics[0])
                 {
-                    picPlaces[2].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[2].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[2] = true;
                     EventManager.Instance.AddScore(25);
                 }
                 break;
-            case 7:
+            case 6:
                 if (picPlaces[3].GetComponent<SpriteRenderer>().sprite == picPlacePics[0])
                 {
-                    picPlaces[3].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+                    picPlaces[3].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[1];
                     sucessess[3] = true;
                     EventManager.Instance.AddScore(25);
                 }
@@ -206,7 +209,7 @@ public class Screen2Controller : MonoBehaviour
         for (int i = 0; i < picPlaces.Length; i++)
         {
             picPlaces[i].transform.GetChild(0).gameObject.SetActive(false);
-            picPlaces[i].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.blue;
+            picPlaces[i].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[0];
 
             switch (Random.Range(0, 10))
             {
@@ -229,17 +232,43 @@ public class Screen2Controller : MonoBehaviour
 
         switch (step)
         {
-            case 4:
+            case 3:
                 picPlaces[0].transform.GetChild(0).gameObject.SetActive(true);
                 break;
-            case 5:
+            case 4:
+
+                if (sucessess[0] == false)
+                {
+                    picPlaces[0].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[2];
+                }
+
                 picPlaces[1].transform.GetChild(0).gameObject.SetActive(true);
                 break;
-            case 6:
+            case 5:
+
+                if (sucessess[1] == false)
+                {
+                    picPlaces[1].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[2];
+                }
+
                 picPlaces[2].transform.GetChild(0).gameObject.SetActive(true);
                 break;
-            case 7:
+            case 6:
+
+                if (sucessess[2] == false)
+                {
+                    picPlaces[2].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[2];
+                }
+
                 picPlaces[3].transform.GetChild(0).gameObject.SetActive(true);
+                break;
+
+            case 7:
+
+                if (sucessess[3] == false)
+                {
+                    picPlaces[3].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = picBackgroundPics[2];
+                }
                 break;
         }
 
